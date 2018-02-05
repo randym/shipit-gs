103c46de680f20406ef7193eb4518d6ead67fc8d
randym <digital.ipseity@gmail.com>
2018-02-05 19:08:18 +0900

    play(update): try using rsync instead of copy

--- src/deploy/update.js
+++ src/deploy/update.js
@@ -2,8 +2,10 @@ import utils from 'shipit-utils';
 import util from 'util';
 import moment from 'moment';
 
-const COPY_TO_CURRENT = 'gsutil -m -q cp -r %s/* %s';
-const COPY_TO_RELEASE = 'gsutil -m -q cp -r %s %s/%d';
+// const COPY_TO_CURRENT = 'gsutil -m -q cp -r %s/* %s';
+// const COPY_TO_RELEASE = 'gsutil -m -q cp -r %s %s/%d';
+const RSYNC_TO_CURRENT = 'gsutil -m -q rsync -r -d %s %s';
+const RSYNC_TO_RELEASE = 'gsutil -m -q rsync -r -d %s %s/%d';
 
 export default function update(shipit) {
 
@@ -11,8 +13,14 @@ export default function update(shipit) {
 
   function task() {
     const timestamp = moment.utc().format('YYYYMMDDHHmmss');
-    const current = util.format(COPY_TO_CURRENT, shipit.config.dirToCopy, shipit.currentPath);
-    const release = util.format(COPY_TO_RELEASE, shipit.currentPath, shipit.releasesPath, timestamp);
+
+    // Still investigating the performance between cp and rsync
+    // const current = util.format(COPY_TO_CURRENT, shipit.config.dirToCopy, shipit.currentPath);
+    // const release = util.format(COPY_TO_RELEASE, shipit.currentPath, shipit.releasesPath, timestamp);
+
+
+    const current = util.format(RSYNC_TO_CURRENT, shipit.config.dirToCopy, shipit.currentPath);
+    const release = util.format(RSYNC_TO_RELEASE, shipit.config.dirToCopy, shipit.currentPath, timestamp);
 
     return shipit.local(current).then(() => {
       return shipit.local(release);
